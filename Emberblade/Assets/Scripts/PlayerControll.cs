e07a35a69bac8be69493279cc3629af585929cac
BrandonMach <brandon.mach2003@gmail.com>
2022-03-22 19:31:00 +0100

    Can jump
    Small jump ring effect

--- Emberblade/Assets/Scripts/PlayerControll.cs
+++ Emberblade/Assets/Scripts/PlayerControll.cs
@@ -8,6 +8,13 @@ public class PlayerControll : MonoBehaviour
     public float movementSpeed = 1;
     private Rigidbody2D player_Rb;
 
+    public float jumpforce = 1;
+    private int jumpCounter = 0;
+    public bool isOnGround = false;
+    public GameObject jumpRingPrefab;
+    public float jRingSpawnTime = 0.1f;
+
+
     void Start()
     {
         player_Rb = GetComponent<Rigidbody2D>();
@@ -17,6 +24,7 @@ public class PlayerControll : MonoBehaviour
     void Update()
     {
         Move();
+        Jump();
     }
 
     void Move()
@@ -24,5 +32,46 @@ public class PlayerControll : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveBy = moveX * movementSpeed;
         player_Rb.velocity = new Vector2(moveBy, player_Rb.velocity.y);
+    }
+
+    void Jump()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCounter < 2 )
+        {
+           
+            player_Rb.velocity = new Vector2(player_Rb.velocity.x, jumpforce);
+            jumpCounter++;
+            isOnGround = false;
+            StartCoroutine(JumpGraphic());
+
+        }
+      
+      
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOnGround = true;
+        jumpCounter = 0;
+    }
+
+    private void SpawnJumpRing()
+    {
+        GameObject a = Instantiate(jumpRingPrefab) as GameObject;
+        a.transform.position = new Vector2(this.transform.position.x, this.transform.position.y - 2.5f);
+    }
+
+    IEnumerator JumpGraphic()
+    {
+        if(!isOnGround && jumpCounter == 1)
+        {
+            yield return new WaitForSeconds(jRingSpawnTime);
+
+            SpawnJumpRing(); 
+            
+        }
+        
     }
 }
