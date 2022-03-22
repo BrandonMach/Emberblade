7f04588918f7fd4c9423a5074302287a3936482b
BrandonMach <brandon.mach2003@gmail.com>
2022-03-22 17:55:59 +0100

    Can move side to side

--- Emberblade/Assets/Scripts/PlayerControll.cs
+++ Emberblade/Assets/Scripts/PlayerControll.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class PlayerControll : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float movementSpeed = 1;
+    private Rigidbody2D player_Rb;
+
     void Start()
     {
-        
+        player_Rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveBy = moveX * movementSpeed;
+        player_Rb.velocity = new Vector2(moveBy, player_Rb.velocity.y);
     }
 }
