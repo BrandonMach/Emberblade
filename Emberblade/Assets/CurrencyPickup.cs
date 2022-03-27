30e6953cb1216371a34727b64f3cc5fad84ed4ba
SniffeKliffe <philip.c525@gmail.com>
2022-03-27 21:09:02 +0200

    gfdh

--- Emberblade/Assets/CurrencyPickup.cs
+++ Emberblade/Assets/CurrencyPickup.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class CurrencyPickup : MonoBehaviour
 {
+<<<<<<< Updated upstream
 
     public int worth = 100;
     public Currency currency;
 
+=======
+    public int worth = 100;
+    public Currency currency;
+>>>>>>> Stashed changes
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -16,4 +21,8 @@ public class CurrencyPickup : MonoBehaviour
             currency.IncreaseCurrency(worth);
         }
     }
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 }
