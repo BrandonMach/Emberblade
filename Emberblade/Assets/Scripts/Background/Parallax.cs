204d38916bee93b97b0c5dd601bcc0895b6fe3dd
Saitama3728 <89905719+Saitama3728@users.noreply.github.com>
2022-04-21 23:25:10 +0200

    Parallax effect underground

--- Emberblade/Assets/Scripts/Background/Parallax.cs
+++ Emberblade/Assets/Scripts/Background/Parallax.cs
@@ -7,12 +7,14 @@ public class Parallax : MonoBehaviour
     private float length, startpos;
     public GameObject cam;
     public float parallaxEffect;
+    PlayerControll player;
 
     // Start is called before the first frame update
     void Start()
     {
         startpos = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
+        player = FindObjectOfType<PlayerControll>();  
     }
 
     // Update is called once per frame
@@ -21,7 +23,10 @@ public class Parallax : MonoBehaviour
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
 
+
         transform.position = new Vector2(startpos + dist, transform.position.y);
+
+        
 
         if (temp > startpos + length)
         {
@@ -31,5 +36,7 @@ public class Parallax : MonoBehaviour
         {
             startpos -= length;
         }
+
+        
     }
 }
