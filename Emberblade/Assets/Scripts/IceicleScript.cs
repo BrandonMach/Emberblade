e42d4237b6aed6c4d2c3eaad610e9ca6669a3dfe
BrandonMach <brandon.mach2003@gmail.com>
2022-04-27 18:28:32 +0200

    icicle script

--- Emberblade/Assets/Scripts/IceicleScript.cs
+++ Emberblade/Assets/Scripts/IceicleScript.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class IceicleScript : MonoBehaviour
 {
+    private LineRenderer lineRenderer;
+    public Transform hitPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        lineRenderer = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit2D playerDetectionLaser = Physics2D.Raycast(transform.position, -transform.up);
+        Debug.DrawLine(transform.position, playerDetectionLaser.point);
+        hitPos.position = playerDetectionLaser.point;
+        lineRenderer.SetPosition(0, hitPos.position);
+        lineRenderer.SetPosition(1, transform.position);
+        lineRenderer.enabled = true;
+        //Om player är i laser ramla
     }
 }
