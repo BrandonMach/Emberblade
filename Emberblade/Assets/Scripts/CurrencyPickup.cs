133fdff9cb0547c1ef9ce9ab3b7bf02764c6b29a
BrandonMach <brandon.mach2003@gmail.com>
2022-04-29 22:04:03 +0200

    Currency script fixed
    Gjort det bättre för prefab behöver inte lägga till spelaren i varje flygga

--- Emberblade/Assets/Scripts/CurrencyPickup.cs
+++ Emberblade/Assets/Scripts/CurrencyPickup.cs
@@ -7,7 +7,12 @@ public class CurrencyPickup : MonoBehaviour
 
 
     public int worth = 100;
-    public Currency currency;
+     Currency currency;
+
+    private void Start()
+    {
+        currency = GameObject.Find("Player").GetComponent<Currency>();
+    }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
