e46a232a8f99f60c2af6ba200bc2eed37d22cf81
AJervill <axeljervill@gmail.com>
2022-05-08 22:31:56 +0200

    another store update
    Lade till lite mer funktionalitet, nu är det så att om man köper det ena i affären, så flyttas det sista objektet till mitten.
    
    Nu kan man även köpa "increase max energy"

--- Emberblade/Assets/Scripts/NPCScripts/StoreScript.cs
+++ Emberblade/Assets/Scripts/NPCScripts/StoreScript.cs
@@ -7,21 +7,34 @@ public class StoreScript : MonoBehaviour
 {
     public bool boughtHealth, boughtEnergy;
     public GameObject healthButton, energyButton;
+    private float middleOfScreenX, middleOfScreenY;
+
+    private void Start()
+    {
+        middleOfScreenX = Screen.width / 2;
+        middleOfScreenY = Screen.height / 2;   
+    }
     void Update()
     {
         if (boughtHealth)
         {
             healthButton.SetActive(false);
+            energyButton.transform.position = new Vector2(middleOfScreenX,middleOfScreenY);
+        }
+        if (boughtEnergy)
+        {
+            energyButton.SetActive(false);
+            healthButton.transform.position = new Vector2(middleOfScreenX,middleOfScreenY); 
         }
     }
 
     public void buyEnergy()
     {
-        if (GameObject.Find("FrogMainCharacterV1").GetComponent<Currency>().currency >= 2)
+        if (GameObject.Find("FrogMainCharacterV1").GetComponent<Currency>().currency >= 100)
         {
-            GameObject.Find("FrogMainCharacterV1").GetComponent<Currency>().currency -= 2;
-            GameObject.Find("FrogMainCharacterV1").GetComponent<PlayerInfo>().maxHealth = (GameObject.Find("FrogMainCharacterV1").GetComponent<PlayerInfo>().maxHealth * 125) / 100;
-            boughtHealth = true;
+            GameObject.Find("FrogMainCharacterV1").GetComponent<Currency>().currency -= 100;
+            GameObject.Find("FrogMainCharacterV1").GetComponent<PlayerInfo>().maxEnergy = (GameObject.Find("FrogMainCharacterV1").GetComponent<PlayerInfo>().maxEnergy * 125) / 100;
+            boughtEnergy = true;
         }
     }
 
