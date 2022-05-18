6f38b64d6660128c2102fe496d1684368053c54d
AJervill <axeljervill@gmail.com>
2022-05-18 14:15:04 +0200

    added wind

---
+++ Emberblade/Assets/Scripts/WindScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    bool affectedByWind = false;
+    float velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (affectedByWind)
+        {
+            velocity += Time.deltaTime;
+            rb.AddForce(Vector2.right * velocity * 80);
+            Debug.Log("hehehehehehehehe");
+            if (velocity >= 80)
+            {
+                velocity = 80;
+            }
+        }
+        else
+        {
+            velocity = 0;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            affectedByWind = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            affectedByWind = false;
+        }
+    }
+}
